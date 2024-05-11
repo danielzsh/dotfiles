2d2e2ac2f672e2558f052afc26b5192e1f37c485
Daniel Zhu <danielfangzhu@gmail.com>
2024-05-10 23:00:03 -0700

    clean up snippets stuff

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -4,7 +4,7 @@ return {
     "rafamadriz/friendly-snippets",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_snipmate").lazy_load({ paths = "./snippets" })
+      require("luasnip.loaders.from_snipmate").lazy_load()
     end,
   },
 }
