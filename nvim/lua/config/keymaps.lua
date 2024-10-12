ea7cc5b3d958950730d4716134fe0819ef548cb4
Daniel Zhu <danielfangzhu@gmail.com>
2024-10-12 12:06:44 -0700

    update keybinds

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>ac", function()
+  vim.cmd("!./run.sh % > run.out")
+  vim.cmd("e run.out")
+end, { desc = "Run file" })
+
+vim.keymap.set("n", "<leader>cc", function()
+  vim.cmd("!./compile.sh %")
+end, { desc = "Preprocess file" })
